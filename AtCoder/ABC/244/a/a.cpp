a8d4c8df2ad518fe6740af9eeaf4c1466868ef99
Jeffrey Ahn <ahjeffre@amazon.com>
2022-03-20 04:57:10 -0700

    adding codestrike and atcoder dumps

---
+++ AtCoder/ABC/244/a/a.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+#define print(x) cout << (#x) << " is " << (x) << endl;
+#define printstd(x)                                                           \
+    for (auto asdf = x.begin(); asdf != x.end(); asdf++) cout << *asdf << ' '; \
+    cout << endl;
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define pb push_back
+#define eb emplace_back
+#define all(a) a.begin(), a.end()
+#define sz(v) ((int)(v).size())
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define inf 0x3f3f3f3f
+#define nl '\n'
+using namespace std;
+
+signed main() {
+    fastio;
+}
